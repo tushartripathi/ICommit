3949bdb0b7e8af31a422912be4b2af5e48fa0ad8
tushartripathi <tushar.tripathi@p360.com>
2025-01-13 00:01:58 +0530

    rout udparted

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(logger);
 
 // Routes
-app.use('/api/users', userRoutes);
+app.use('/api', userRoutes);
 
 
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,21 @@ const express = require('express');
 const { getUsers } = require('../controllers/userController');
 const router = express.Router();
 
+const sendJson = (req, res) => {
+    // Example data to send as JSON
+    const users = [
+      { id: 1, name: "John Doe" },
+      { id: 2, name: "Jane Smith" }
+    ];
+  
+    // Send JSON response
+    res.json({ success: true, data: users });
+  };
+
+  
 router.get('/', getUsers);
+
+router.get('/to', sendJson);
 
 
 
